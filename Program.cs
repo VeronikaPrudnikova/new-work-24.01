cc764b24db5ca5e58123e66a8a0862ae1e09e971
Veronika <prudnikovaveronika7@gmail.com>
2023-01-27 17:17:21 +0300

    решение домашнего задания еще 1 вариант

--- Program.cs
+++ Program.cs
@@ -37,3 +37,22 @@ ShowArray(newArray, size);
 
 
 
+/*
+string[] Array;
+Console.WriteLine("Введите массив строк, через пробел");
+string stroka = Console.ReadLine();
+Array = stroka.Split(' ');
+var result = new string[Array.Length];
+var realSize = 0;
+foreach(var value in Array)
+{
+    if(value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
+*/
